0f4f9b09ace3a9e42dfe6a530c768f9d0a88014d
nikolaprifti <126162944+nikolaprifti@users.noreply.github.com>
2023-10-21 13:36:27 +0100

    commit

---
+++ src/main/java/com/car/rental/service/controllers/EmployeeController.java
@@ -0,0 +1,40 @@
+package com.car.rental.service.controllers;
+
+
+import com.car.rental.service.dao.Employee;
+import com.car.rental.service.services.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+@RequiredArgsConstructor
+public class EmployeeController {
+    private final EmployeeService employeeService;
+    @GetMapping("/all")
+    public List<Employee> findAll() {
+        return employeeService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee getById(@PathVariable Long id) {
+        return employeeService.findById(id);
+    }
+
+    @PostMapping("/create")
+    public Employee create(@RequestBody Employee employee) {
+        return employeeService.create(employee);
+    }
+
+    @PutMapping("/update")
+    public Employee update(@RequestBody Employee employee) {
+        return employeeService.update(employee);
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteById(@RequestParam Long id) {
+        return employeeService.delete(id);
+    }
+}
--- src/main/java/com/car/rental/service/static_data/Role.java
+++
@@ -1,6 +0,0 @@
-package com.car.rental.service.static_data;
-
-public enum Role {
-    ROLE_EMPLOYEE,
-    ROLE_MANAGER;
-}
