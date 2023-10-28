102be027917616b01d252554cbe4096d68016a87
nikolaprifti <126162944+nikolaprifti@users.noreply.github.com>
2023-10-28 13:19:16 +0100

    FE development

--- car-rental-fe/src/app/app-routing.module.ts
+++ car-rental-fe/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
+import {EmployeeComponent} from "./components/employee/employee.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'employees',component:EmployeeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
