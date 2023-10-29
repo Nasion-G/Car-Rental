a0f1b853f2957a9282e36cb07fc9efd9ece8bde1
nikolaprifti <126162944+nikolaprifti@users.noreply.github.com>
2023-10-29 09:45:43 +0000

    sfond

--- car-rental-fe/src/app/app-routing.module.ts
+++ car-rental-fe/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   {path:'',component:LoginComponent},
   {path:'register',component:RegisterComponent},
   {path:'employees',component:EmployeeComponent},
-  {}
+
 ];
 
 @NgModule({
