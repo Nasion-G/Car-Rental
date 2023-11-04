aacc9081e281603c77d9d614a006f1e662c50d70
nikolaprifti <126162944+nikolaprifti@users.noreply.github.com>
2023-11-04 09:47:57 +0000

    commit

--- car-rental-fe/src/app/components/manager/manager.component.ts
+++ car-rental-fe/src/app/components/manager/manager.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Employee} from "../../models/employee";
+import {EmployeeService} from "../../services/employee.service";
 
 @Component({
   selector: 'app-manager',
   templateUrl: './manager.component.html',
   styleUrls: ['./manager.component.css']
 })
-export class ManagerComponent {
+export class ManagerComponent implements OnInit{
+  employees: Employee;
+  constructor(private employeeService:EmployeeService) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
+
+
